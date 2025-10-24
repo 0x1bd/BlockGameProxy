03baa7c8b104e8b0fab03d7f285cd99deba1d552
kvxd <redslyice@outlook.com>
2025-10-25 00:33:21 +0200

    refactor: Cleanup PacketHandlerRegistries

--- src/main/kotlin/org/kvxd/blockgameproxy/core/handler/PacketHandlerRegistries.kt
+++ src/main/kotlin/org/kvxd/blockgameproxy/core/handler/PacketHandlerRegistries.kt
@@ -14,28 +14,31 @@ object PacketHandlerRegistries {
     fun initialize() {
         LOGGER.debug("Registering packet handlers")
 
-        CLIENT.registerIncoming(CFinishConfigurationHandler())
-        CLIENT.registerIncoming(CKeepAliveHandler())
-        CLIENT.registerIncoming(CLoginCompressionHandler())
-        CLIENT.registerIncoming(CLoginFinishedHandler())
-        CLIENT.registerIncoming(CCLoginHandler())
-        CLIENT.registerIncoming(CCommandsHandler())
-        CLIENT.registerIncoming(CPlayerAbilitiesHandler())
-        CLIENT.registerIncoming(CRegistryDataHandler())
-        CLIENT.registerIncoming(CSelectKnownPacks())
-        CLIENT.registerIncoming(CSetHeldSlotHandler())
-        CLIENT.registerIncoming(CUpdateTagsHandler())
-
-        SERVER.registerIncoming(SFinishConfigurationHandler())
-        SERVER.registerIncoming(SHelloHandler())
-        SERVER.registerIncoming(SIntentHandler())
-        SERVER.registerIncoming(SKeyHandler())
-        SERVER.registerIncoming(SLoginAckHandler())
-        SERVER.registerIncoming(SPingHandler())
-        SERVER.registerIncoming(SStatusHandler())
-
-        //TODO: Add remaining handlers (position, light data, chunks etc.)
-        //TODO: Generalize shared data. (NO SharedData singleton, instead BlockData, PlayerData, LoginData and so on.)
+        CLIENT.registerClientIncoming()
+        SERVER.registerServerIncoming()
     }
 
+    private fun PacketHandlerRegistry.registerClientIncoming() {
+        registerIncoming(CFinishConfigurationHandler())
+        registerIncoming(CKeepAliveHandler())
+        registerIncoming(CLoginCompressionHandler())
+        registerIncoming(CLoginFinishedHandler())
+        registerIncoming(CCLoginHandler())
+        registerIncoming(CCommandsHandler())
+        registerIncoming(CPlayerAbilitiesHandler())
+        registerIncoming(CRegistryDataHandler())
+        registerIncoming(CSelectKnownPacks())
+        registerIncoming(CSetHeldSlotHandler())
+        registerIncoming(CUpdateTagsHandler())
+    }
+
+    private fun PacketHandlerRegistry.registerServerIncoming() {
+        registerIncoming(SFinishConfigurationHandler())
+        registerIncoming(SHelloHandler())
+        registerIncoming(SIntentHandler())
+        registerIncoming(SKeyHandler())
+        registerIncoming(SLoginAckHandler())
+        registerIncoming(SPingHandler())
+        registerIncoming(SStatusHandler())
+    }
 }
